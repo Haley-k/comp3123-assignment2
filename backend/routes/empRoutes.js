24ba85fb60e80c92054f9afc5cf4e2bc12de1015
Haley <hs.haleyk@gmail.com>
2024-11-26 22:53:22 -0500

    revisioned for assignment2

---
+++ backend/routes/empRoutes.js
@@ -0,0 +1,111 @@
+const express = require('express');
+const Employee = require('../models/empModel');
+const router = express.Router();
+
+// Get all employees
+router.get('/employees', async (req, res) => {
+    try {
+        const employees = await Employee.find();
+        res.status(200).json(employees);
+    } catch (error) {
+        res.status(500).json({ message: 'Server error' });
+    }
+});
+
+// Create new employee
+router.post('/employees', async (req, res) => {
+    const { first_name, last_name, email, position, salary, date_of_joining, department } = req.body;
+    try {
+        const newEmployee = new Employee({
+            first_name,
+            last_name,
+            email,
+            position,
+            salary,
+            date_of_joining,
+            department,
+            created_at: new Date(),
+            updated_at: new Date()
+        });
+
+        await newEmployee.save();
+
+        res.status(201).json({
+            message: 'Employee created successfully',
+            employee_id: newEmployee._id
+        });
+    } catch (error) {
+        res.status(500).json({ message: 'Server error' });
+    }
+});
+
+// Get employee by ID
+router.get('/employees/:eid', async (req, res) => {
+    try {
+        const employee = await Employee.findById(req.params.eid);
+        if (!employee) {
+            return res.status(404).json({ message: 'Employee not found' });
+        }
+        res.status(200).json(employee);
+    } catch (error) {
+        res.status(500).json({ message: 'Server error' });
+    }
+});
+
+// Update employee by ID
+router.put('/employees/:eid', async (req, res) => {
+    const updates = req.body;
+    try {
+        const employee = await Employee.findByIdAndUpdate(req.params.eid, updates, { new: true });
+        if (!employee) {
+            return res.status(404).json({ message: 'Employee not found' });
+        }
+        res.status(200).json({ message: 'Employee details updated successfully' });
+    } catch (error) {
+        res.status(500).json({ message: 'Server error' });
+    }
+});
+
+// Delete employee by ID
+router.delete('/employees', async (req, res) => {
+    const { eid } = req.query;
+
+    try {
+        const employee = await Employee.findByIdAndDelete(eid);
+        if (!employee) {
+            return res.status(404).json({ message: 'Employee not found' });
+        }
+        res.status(200).json({ message: 'Employee deleted successfully' });
+    } catch (error) {
+        res.status(500).json({ message: 'Server error' });
+    }
+});
+
+// search employees
+router.get("/search", async (req, res) => {
+    const { department, position } = req.query;
+
+    try {
+        const query = {};
+
+        if (department && department.trim() !== "") {
+            query.department = new RegExp(department, "i"); 
+        }
+        if (position && position.trim() !== "") {
+            query.position = new RegExp(position, "i");
+        }
+
+        const employees = await Employee.find(query);
+
+        if (employees.length === 0) {
+            return res.status(404).json({ message: "No employees found." });
+        }
+
+        res.status(200).json(employees);
+    } catch (error) {
+        console.error("Error searching employees:", error);
+        res.status(500).json({ message: "Server error" });
+    }
+});
+
+module.exports = router;
---
+++ backend/server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require("cors");
+require('dotenv').config();
+
+
+const SERVER_PORT = process.env.PORT || 8000;
+const mongo_URI = process.env.MONGO_URI;
+
+const app = express();
+
+app.use(cors());
+
+(async () => {
+  try {
+      if (!mongo_URI) {
+          throw new Error('MONGO_URI not found');
+      }
+      
+      await mongoose.connect(mongo_URI);
+      console.log('MongoDB connected successfully');
+  } catch (err) {
+      console.error('MongoDB connection failed:', err.message);
+      process.exit(1);
+  }
+})();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const userRouter = require('./routes/userRoutes');
+const empRouter = require('./routes/empRoutes');
+
+app.use('/api/v1/user', userRouter);
+app.use('/api/v1/emp', empRouter);
+
+app.listen(SERVER_PORT, () => {
+  console.log(`Server is running on port ${SERVER_PORT}`);
+});
+
+module.exports = app;
